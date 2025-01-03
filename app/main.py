3090c39398d50f7335fe4310b38a8348f313b82e
yasunaga shuto <yasunaga.shuto@gmail.com>
2025-01-03 15:17:39 +0900

    作成

--- app/main.py
+++ app/main.py
@@ -15,8 +15,8 @@ ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
 
 PAI = (
   '1m', '2m', '3m', '4m', '5m', '5mRed', '6m', '7m', '8m', '9m',
-  '1s', '2s', '3s', '4s', '5s', '5sRed', '6s', '7s', '8s', '9s',
   '1p', '2p', '3p', '4p', '5p', '5pRed', '6p', '7p', '8p', '9p',
+  '1s', '2s', '3s', '4s', '5s', '5sRed', '6s', '7s', '8s', '9s',
   'ton', 'nan', 'sha', 'pei', 'haku', 'hatsu', 'chun'
 )
 ORDER = {}
@@ -41,23 +41,40 @@ def send_tile_dealing():
   # )
   # client.create_tweet(text="テスト")
 
+  # ドラ表示牌
+  dra_indicator, count_key = random_tile()
+  PAI_COUNT[count_key] -= 1
+  # 配牌
   tiles = select_tiles()
   tiles.sort(key=lambda val: ORDER[val])
+
+  # ツモ牌
+  drawn_tile = draw_tile()
 
   background = Image.open('img/background.webp')
   for i, t in enumerate(tiles):
     img = Image.open(f'img/{t}.webp').resize((36, 49))
-    background.paste(img, (i * 36 + 30, 110))
+    background.paste(img, (i * 36 + 30, 130))
 
-  background.save('test.webp', quality=95)
+  drawn_tile_img = Image.open(f'img/{drawn_tile}.webp').resize((36, 49))
+  background.paste(drawn_tile_img, (508, 130))
+
+  dra_indicator_img = Image.open(f'img/{dra_indicator}.webp').resize((36, 49))
+  back = Image.open('img/back.webp').resize((36, 49))
+
+  for i in range(7):
+    if i == 2:
+      background.paste(dra_indicator_img, (i * 36 + 270, 35))
+    else:
+      background.paste(back, (i * 36 + 270, 35))
+
+  background.save('test.webp', quality=100)
   print(PAI_COUNT)
 
 def select_tiles():
   tiles = []
   while len(tiles) < 13:
-    num = random.randint(0, len(PAI) - 1)
-    append_tile = PAI[num]
-    count_key = append_tile.replace('Red', '')
+    append_tile, count_key = random_tile()
     # 同じ赤が2枚以上入らないようにする
     if 'Red' in append_tile and append_tile in tiles:
       continue
@@ -65,10 +82,23 @@ def select_tiles():
     if PAI_COUNT[count_key] == 0:
       continue
 
-    tiles.append(PAI[num])
+    tiles.append(append_tile)
     PAI_COUNT[count_key] -= 1
 
   return tiles
+
+def random_tile():
+  num = random.randint(0, len(PAI) - 1)
+  tile = PAI[num]
+  return tile, tile.replace('Red', '')
+
+def draw_tile():
+  while True:
+    tile, count_key = random_tile()
+    if PAI_COUNT[count_key] > 0:
+      break
+
+  return tile
 
 if __name__ == '__main__':
   send_tile_dealing()
